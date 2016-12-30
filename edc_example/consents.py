e89b7f8a254c4d1c2081520468c1339679b9d599
erikvw <ew2789@gmail.com>
2016-12-31 01:42:54 +0200

    consent

--- edc_example/consents.py
+++ edc_example/consents.py
@@ -1,11 +1,16 @@
+from django.apps import apps as django_apps
+
 from edc_consent.consent import Consent
 from edc_consent.site_consents import site_consents
 from edc_constants.constants import MALE, FEMALE
 
+app_config = django_apps.get_app_config('edc_protocol')
 
 subjectconsent_v1 = Consent(
     'edc_example.subjectconsent',
     version='1',
+    start=app_config.study_open_datetime,
+    end=app_config.study_close_datetime,
     age_min=16,
     age_is_adult=18,
     age_max=64,
